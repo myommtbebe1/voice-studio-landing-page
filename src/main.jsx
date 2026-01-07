f3b1fe2b497f1a9d7d50e3748d742e882bb3e563
Myo Myat Thu <myomyatthu123321@gmail.com>
2026-01-07 14:24:44 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import './index.css'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <App />
+ 
+)
